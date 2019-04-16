a88aa7a5f4bce874708c836a553383982f5e7302
Chip Cerio <alistair.cerio@gmail.com>
2019-04-16 22:46:44 +0800

    navigationOptions on TabNavigator

--- src/screens/Home/Tabs.container.js
+++ src/screens/Home/Tabs.container.js
@@ -5,11 +5,22 @@ import People from '../People/People.container';
 import Notifications from '../Notifications/Notifications.container';
 
 // https://github.com/react-navigation/react-navigation/blob/master/examples/NavigationPlayground/app/CustomTabUI.tsx
-const TabLayout = createBottomTabNavigator({
+const TabNavigator = createBottomTabNavigator({
   Messages,
   Feeds,
   People,
   Notifications,
 });
 
-export default TabLayout;
+TabNavigator.navigationOptions = ({ navigation }) => {
+  const { routeName } = navigation.state.routes[navigation.state.index];
+
+  // You can do whatever you like here to pick the title based on the route name
+  const headerTitle = routeName;
+
+  return {
+    headerTitle,
+  };
+};
+
+export default TabNavigator;
