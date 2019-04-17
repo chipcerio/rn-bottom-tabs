9f0bd1faf7de5fb05a1c63b7d670bb8bc514d47a
Chip Cerio <alistair.cerio@gmail.com>
2019-04-17 18:35:27 +0800

    Add header title on Messages tab

--- src/screens/Home/Tabs.container.js
+++ src/screens/Home/Tabs.container.js
@@ -1,26 +1,46 @@
-import { createBottomTabNavigator } from 'react-navigation';
+import React from 'react';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { ListItem, Icon } from 'react-native-elements';
 import Messages from '../Messages/Messages.container';
+import MessageDetails from '../Messages/Details.container';
 import Feeds from '../Feeds/Feeds.container';
 import People from '../People/People.container';
 import Notifications from '../Notifications/Notifications.container';
 
+const MessagesStack = createStackNavigator(
+  {
+    MessagesList: Messages,
+    Details: MessageDetails,
+  },
+  {
+    navigationOptions: () => ({
+      tabBarLabel: 'Messages',
+      tabBarIcon: ({ tintColor, focused, horizontal }) => (
+        <Icon
+          name={focused ? 'message' : 'message'}
+          size={horizontal ? 20 : 26}
+          iconStyle={{ color: tintColor }}
+          type="MaterialIcons"
+        />
+      ),
+    }),
+  },
+);
+
 // https://github.com/react-navigation/react-navigation/blob/master/examples/NavigationPlayground/app/CustomTabUI.tsx
-const TabNavigator = createBottomTabNavigator({
-  Messages,
-  Feeds,
-  People,
-  Notifications,
-});
-
-TabNavigator.navigationOptions = ({ navigation }) => {
-  const { routeName } = navigation.state.routes[navigation.state.index];
-
-  // You can do whatever you like here to pick the title based on the route name
-  const headerTitle = routeName;
-
-  return {
-    headerTitle,
-  };
-};
+const TabNavigator = createBottomTabNavigator(
+  {
+    Messages: MessagesStack,
+    Feeds,
+    People,
+    Notifications,
+  },
+  {
+    navigationOptions: options => {
+      console.log(options);
+      return { header: null };
+    },
+  },
+);
 
 export default TabNavigator;
--- src/screens/Messages/Messages.container.js
+++ src/screens/Messages/Messages.container.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
-import { ListItem, Icon } from 'react-native-elements';
+import { ListItem } from 'react-native-elements';
 import { CHEESES } from '../../constants';
 
 export class MessagesContainer extends Component {
   static navigationOptions = {
-    tabBarLabel: 'Messages',
-    tabBarIcon: ({ tintColor, focused, horizontal }) => (
-      <Icon
-        name={focused ? 'message' : 'message'}
-        size={horizontal ? 20 : 26}
-        iconStyle={{ color: tintColor }}
-        type="MaterialIcons"
-      />
-    ),
+    title: 'Hola',
+    header: null,
   };
 
-  renderItem = ({ item }) => <ListItem key={item} title={item} onPress={() => {}} />;
+  renderItem = ({ item }) => (
+    <ListItem key={item} title={item} onPress={() => this.props.navigation.navigate('Details')} />
+  );
 
   render() {
     return <FlatList data={CHEESES} renderItem={this.renderItem} keyExtractor={item => item} />;
